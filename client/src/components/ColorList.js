e0c3eb644d43b7acbc05976182b459611b6dc8c5
Kate McGee <mcgeehee@gmail.com>
2020-05-22 10:32:56 -0600

    saveEdit function working and saving new color successfully

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,48 +1,70 @@
 import React, { useState } from "react";
 import axios from "axios";
-import{axiosWithAuth} from '../utils/axiosWithAuth';
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory, useParams } from "react-router-dom";
 
 const initialColor = {
   color: "",
-  code: { hex: "" }
+  code: { hex: "" },
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  console.log("colors inside color list", colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
   const [newColor, setNewColor] = useState(initialColor);
+  const [loading, setIsLoading] = useState(false);
 
+  const { push } = useHistory();
+  const { id } = useParams();
 
-  const editColor = color => {
+  const editColor = (color) => {
     setEditing(true);
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveEdit = (e) => {
     e.preventDefault();
-    // Make a put request to save your updated color
+    axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+
+      // Make a put request to save your updated color
+      .then((res) => {
+        setIsLoading(true);
+        updateColors([...colors, res.data]);
+        setEditing(false)
+         setTimeout(function () {
+          setIsLoading(false);
+          push('/protected')
+        }, 2000);
+      })
+      .catch((err) => console.log(err));
+
     // think about where will you get the id from...
     // where is is saved right now?
   };
 
-  const deleteColor = color => {
+  const deleteColor = (color) => {
     // make a delete request to delete this color
   };
 
   return (
     <div className="colors-wrap">
       <p>colors</p>
+      {loading ? "Loading..." : 
       <ul>
-        {colors.map(color => (
+       
+        {colors.map((color) => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -52,14 +74,14 @@ const ColorList = ({ colors, updateColors }) => {
             />
           </li>
         ))}
-      </ul>
+      </ul>}
       {editing && (
         <form onSubmit={saveEdit}>
           <legend>edit color</legend>
           <label>
             color name:
             <input
-              onChange={e =>
+              onChange={(e) =>
                 setColorToEdit({ ...colorToEdit, color: e.target.value })
               }
               value={colorToEdit.color}
@@ -68,10 +90,10 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             hex code:
             <input
-              onChange={e =>
+              onChange={(e) =>
                 setColorToEdit({
                   ...colorToEdit,
-                  code: { hex: e.target.value }
+                  code: { hex: e.target.value },
                 })
               }
               value={colorToEdit.code.hex}
